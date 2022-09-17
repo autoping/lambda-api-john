d3e511a86f74b60dc88171720ee92046c45dd666
achunin <aleksandr.chunin@t-systems.com>
2022-09-17 13:51:54 +0300

    serverless with dd

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lambda API John
 API lambda handler used by John actors
+
+
+### for local deploy use the following command
+_serverless offline start_
--- handler.js
+++ handler.js
@@ -1,7 +1,40 @@
 'use strict';
 
+const AWS = require("aws-sdk");
+
+const messagesTableName = "messages";
+
+//to use for local and prod
+const dynamodb = require('serverless-dynamodb-client');
+const docClient = dynamodb.doc;
+
+//temp for local rn
+
+
+// const docClient = new AWS.DynamoDB.DocumentClient({
+//     region: 'localhost',
+//     endpoint: 'http://localhost:8000',
+//     accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
+//     secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
+// });
+
+
+module.exports.getMessages = async function (fingerPrint, cardId) {
+  let params = {
+    TableName: messagesTableName,
+    FilterExpression: " fingerPrint = :f AND cardId = :c ",
+    ExpressionAttributeValues: {
+      ":f": fingerPrint,
+      ":c": cardId
+    }
+  }
+  return await docClient.scan(params).promise();
+};
+
 module.exports.getMessages = async (event) => {
   console.log("Get messages request received: " + JSON.stringify(event.body));
+  console.log(event);
+  // let m = getMessages(event.);
   return {
     statusCode: 200,
     body: JSON.stringify({
