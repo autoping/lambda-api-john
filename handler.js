90b853d5301f8ac57df254e3b4f1149f9961a328
achunin <aleksandr.chunin@t-systems.com>
2022-09-17 16:23:46 +0300

    messages get/post

--- handler.js
+++ handler.js
@@ -1,57 +1,74 @@
 'use strict';
-
+const uuid = require("uuid");
 const AWS = require("aws-sdk");
 
 const messagesTableName = "messages";
 
 //to use for local and prod
-const dynamodb = require('serverless-dynamodb-client');
-const docClient = dynamodb.doc;
+// const dynamodb = require('serverless-dynamodb-client');
+// const docClient = dynamodb.doc;
 
 //temp for local rn
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+    accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
+    secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
+});
 
-
-// const docClient = new AWS.DynamoDB.DocumentClient({
-//     region: 'localhost',
-//     endpoint: 'http://localhost:8000',
-//     accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
-//     secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
-// });
-
-
-module.exports.getMessages = async function (fingerPrint, cardId) {
-  let params = {
-    TableName: messagesTableName,
-    FilterExpression: " fingerPrint = :f AND cardId = :c ",
-    ExpressionAttributeValues: {
-      ":f": fingerPrint,
-      ":c": cardId
-    }
-  }
-  return await docClient.scan(params).promise();
-};
 
 module.exports.getMessages = async (event) => {
-  console.log("Get messages request received: " + JSON.stringify(event.body));
-  console.log(event);
-  // let m = getMessages(event.);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      items: [
-        {
-          id: "3ac2a3f9-b9b7-481c-aa00-32543eaffd32",
-          dialogId: "e6e1aa11-b93e-4371-8213-fa21a50747d8",
-          author: "John",
-          text: "Please move your car",
-          createdAt: 1641619332
-        }
-      ]
-    })
-  }
+    console.log("Get messages request received: " + JSON.stringify(event.queryStringParameters));
+    let m = [];
+    if (event.queryStringParameters
+        && event.queryStringParameters.initiatorId
+        && event.queryStringParameters.cardId) {
+
+        m = await getMessages(event.queryStringParameters.initiatorId, event.queryStringParameters.cardId, +event.queryStringParameters.fromTime||1);
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            items: m
+        })
+    }
 }
 
 module.exports.sendMessage = async (event) => {
-  console.log("Outbound message received: " + JSON.stringify(event.body));
-  return {statusCode: 201};
+    console.log("Outbound message received: " + JSON.stringify(event.body));
+    const messageInput = JSON.parse(event.body);
+    let statusCode = 201;
+
+    const message = {
+        id: uuid.v4(),
+        text: messageInput.text,
+        cardId: messageInput.cardId,
+        initiatorId: messageInput.initiatorId,
+        createdAt: Math.floor(Date.now() / 1000),
+        inbound: true
+    }
+    //todo: validate
+
+    let params = {
+        TableName: messagesTableName,
+        Item: message
+    };
+
+    let result = await docClient.put(params).promise();
+    console.log(result);
+    return {statusCode: statusCode};
 }
+
+const getMessages = async function (initiatorId, cardId, fromTime) {
+    let params = {
+        TableName: messagesTableName,
+        FilterExpression: " initiatorId = :initiatorId AND cardId = :cardId AND createdAt > :fromTime",
+        ExpressionAttributeValues: {
+            ":initiatorId": initiatorId,
+            ":cardId": cardId,
+            ":fromTime": fromTime
+        }
+    }
+    return await docClient.scan(params).promise();
+};
